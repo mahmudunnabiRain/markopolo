3ea8ac703e7db08a481a5aea53141ba6ee7a71e8
mahmudunnabiRain <mahmudunnabirain@gmail.com>
2021-03-13 17:56:34 +0600

    serverless functions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import Header from './components/Header'
 import Admin from './components/Admin'
+import axios from 'axios'
+import { useEffect } from 'react'
 
 function App() {
+
+
+  const fetchData = async () => {
+    const results = await axios.get('/.netlify/functions/helloWorld')
+    console.log(results)
+  }
+
+
+
   return (
     <div className="Container">
       <Header title="Markopolo.Ai"/>
       <Admin />
+      <p></p>
     </div>
   );
 }
